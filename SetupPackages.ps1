d2a6c04b31383e2073b3f4d46fb493df5e4f5e24
Jiri Formacek <jiri.formacek@greycorbel.com>
2023-08-07 12:51:07 +0200

    updated workflow

---
+++ SetupPackages.ps1
@@ -0,0 +1,31 @@
+param
+(
+ [string]$Root
+)
+
+"Restoring packages"
+&"$env:nuget" restore "$Root\packages.config" -packagesDirectory "$Root\packages"
+"Updating packages in the module"
+$packages = ([xml](get-content -path "$Root\packages.config" -raw)).packages.package
+$packages
+$pkg = $packages | where-object{$_.id -eq "Microsoft.Identity.Client"}
+"Processing: $($pkg.id)"
+Copy-Item -Path "$Root\packages\$($pkg.id)`.$($pkg.version)\lib\net461\$($pkg.id)`.dll" -Destination "$Root\Module\AadAuthenticationFactory\shared\net461" -Force
+Copy-Item -Path "$Root\packages\$($pkg.id)`.$($pkg.version)\lib\net6.0\$($pkg.id)`.dll" -Destination "$Root\Module\AadAuthenticationFactory\shared\net6.0" -Force
+
+$pkg = $packages | where-object{$_.id -eq "Microsoft.IdentityModel.Abstractions"}
+"Processing: $($pkg.id)"
+Copy-Item -Path "$Root\packages\$($pkg.id)`.$($pkg.version)\lib\net461\$($pkg.id)`.dll" -Destination "$Root\Module\AadAuthenticationFactory\shared\net461" -Force
+Copy-Item -Path "$Root\packages\$($pkg.id)`.$($pkg.version)\lib\net6.0\$($pkg.id)`.dll" -Destination "$Root\Module\AadAuthenticationFactory\shared\net6.0" -Force
+
+$pkg = $packages | where-object{$_.id -eq "Microsoft.Identity.Client.NativeInterop"}
+"Processing: $($pkg.id)"
+Copy-Item -Path "$Root\packages\$($pkg.id)`.$($pkg.version)\lib\net461\$($pkg.id)`.dll" -Destination "$Root\Module\AadAuthenticationFactory\shared\net461" -Force
+Copy-Item -Path "$Root\packages\$($pkg.id)`.$($pkg.version)\lib\netstandard2.0\$($pkg.id)`.dll" -Destination "$Root\Module\AadAuthenticationFactory\shared\netstandard2.0" -Force
+#runtimes for native interop
+Copy-Item -Path "$Root\packages\$($pkg.id)`.$($pkg.version)\runtimes" -Destination "$Root\Module\AadAuthenticationFactory" -recurse -force
+
+$pkg = $packages | where-object{$_.id -eq "Microsoft.Identity.Client.Broker"}
+"Processing: $($pkg.id)"
+Copy-Item -Path "$Root\packages\$($pkg.id)`.$($pkg.version)\lib\net461\$($pkg.id)`.dll" -Destination "$Root\Module\AadAuthenticationFactory\shared\net461" -Force
+Copy-Item -Path "$Root\packages\$($pkg.id)`.$($pkg.version)\lib\netstandard2.0\$($pkg.id)`.dll" -Destination "$Root\Module\AadAuthenticationFactory\shared\netstandard2.0" -Force
